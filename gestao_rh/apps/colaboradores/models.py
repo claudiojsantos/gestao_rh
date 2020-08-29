7e2f2ee8bd71bc153ff1392fd7accb882b5d69c1
Cláudio Santos <cljos@hotmail.com>
2020-08-29 16:33:35 -0300

    chore: add relationships of models

--- gestao_rh/apps/colaboradores/models.py
+++ gestao_rh/apps/colaboradores/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.departamentos.models import Departamento
+from apps.empresas.models import Empresa
 
 class Colaborador(models.Model):
     nome = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.DO_NOTHING)
 
 
     def __str__(self):
