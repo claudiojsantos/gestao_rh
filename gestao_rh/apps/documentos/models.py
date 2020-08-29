3e78032abb814d61f47247e7935cb5f78a8ed841
Cláudio Santos <cljos@hotmail.com>
2020-08-29 16:33:35 -0300

    chore: add relationships of models

--- gestao_rh/apps/documentos/models.py
+++ gestao_rh/apps/documentos/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from apps.colaboradores.models import Colaborador
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
+    colaborador = models.ForeignKey(Colaborador, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.descricao
