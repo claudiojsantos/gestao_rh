3e02aa88b41aab2c056f4fc6eb72135a6c7f9d60
Claudio Santos <cljos@hotmail.com>
2020-08-30 14:54:51 -0300

    chore: add bootstrap

--- gestao_rh/gestao_rh/urls.py
+++ gestao_rh/gestao_rh/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', include('apps.core.urls')),
     path('colaboradores/', include('apps.colaboradores.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
